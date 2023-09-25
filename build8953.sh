6c087e9a7e1b880cc4ca1faba7d40d5b8a420a87
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-09-25 18:02:54 +0530

    Create build8953.sh

---
+++ build8953.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/addison-experimentations/lk2nd-build-env
+cd lk2nd-build-env
+bash envsetup.sh
+git clone https://github.com/addison-experimentations/lk2nd
+cd lk2nd
+make TOOLCHAIN_PREFIX=arm-none-eabi- msm8953-secondary
+
+
+
+
+rm -rf /opt/gcc-arm-none-eabi
